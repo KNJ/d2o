94c1ed29382979e97895be8a3d3838895c14afa2
KNJ <knj@wazly.net>
2016-02-11 19:39:50 +0900

    Add testGroupFormat()

--- tests/FormatTest.php
+++ tests/FormatTest.php
@@ -5,7 +5,6 @@ class FormatTest extends D2OReady
 {
     public function testObjectFormat()
     {
-        $this->d2o = new D2O("mysql:dbname=test;host=localhost", 'root', 'password');
         $sql = 'SELECT number, symbol, name FROM elements WHERE number <= 3';
         $rows = $this->d2o->state($sql)
             ->run()
@@ -24,6 +23,47 @@ class FormatTest extends D2OReady
             2 => (object)[
                 'number' => '3',
                 'symbol' => 'Li',
+                'name' => 'lithium',
+            ],
+        ], $rows);
+    }
+
+    public function testGroupFormat()
+    {
+        $sql = 'SELECT number, symbol, name FROM elements WHERE number <= 3';
+
+        $rows = $this->d2o->state($sql)
+            ->run()
+            ->format('group');
+        $this->assertArraySubset([
+            '1' => (object)[
+                'symbol' => 'H',
+                'name' => 'hydrogen',
+            ],
+            '2' => (object)[
+                'symbol' => 'He',
+                'name' => 'helium',
+            ],
+            '3' => (object)[
+                'symbol' => 'Li',
+                'name' => 'lithium',
+            ],
+        ], $rows);
+
+        $rows = $this->d2o
+            ->run()
+            ->format('group', ['key' => 'symbol']);
+        $this->assertArraySubset([
+            'H' => (object)[
+                'number' => '1',
+                'name' => 'hydrogen',
+            ],
+            'He' => (object)[
+                'number' => '2',
+                'name' => 'helium',
+            ],
+            'Li' => (object)[
+                'number' => '3',
                 'name' => 'lithium',
             ],
         ], $rows);
